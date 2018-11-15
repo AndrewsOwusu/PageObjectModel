b573352b5081d5eaf691c5b5a92e0a392293f601
AndrewsOwusu <nanarocky47@gmail.com>
2018-11-15 16:57:32 -0600

    updated driver location

--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -20,7 +20,7 @@ public class FreeCrmTest {
 
 	@BeforeMethod
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver_win32.zip\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\_TMP\\testing\\selenium\\chromedriver.exe");
 //		System.setProperty("webdriver.chrome.driver", "C:\\Users\\egorogl\\_TMP\\selenium\\lib\\chromedriver.exe");
 		driver = new ChromeDriver();
 		js = (JavascriptExecutor) driver;
